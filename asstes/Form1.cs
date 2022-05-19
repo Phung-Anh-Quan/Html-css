fe1328b0f3129702aeb0529c0d7c49d21fd5c32f
Phung-Anh-Quan <90770323+Phung-Anh-Quan@users.noreply.github.com>
2022-05-19 08:45:39 +0700

    Bài Cuối kì C#

---
+++ asstes/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace form_1
+{
+    public partial class FrmDn : Form
+    {
+        public FrmDn()
+        {
+            InitializeComponent();
+        }
+
+        private SqlConnection conn;
+        private SqlCommand command;
+        private SqlDataAdapter adapter;
+        private DataSet dataSet;
+        private SqlDataReader reader;
+        private string sqlstr;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrmQL quanly = new FrmQL();
+            quanly.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FrmDk dangky = new FrmDk ();
+            dangky.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ asstes/Quản Lý.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace form_1
+{
+    partial class FrmQL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtHt = new System.Windows.Forms.TextBox();
+            this.TxtQueQuan = new System.Windows.Forms.TextBox();
+            this.dataTNS = new System.Windows.Forms.DateTimePicker();
+            this.CBBoxLV = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(57, 357);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(685, 253);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Họ và Tên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Năm Sinh";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(125, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Quê Quán";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(123, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Phòng Làm Việc";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(456, 310);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(293, 310);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Sửa";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(143, 310);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Xoá";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // txtHt
+            // 
+            this.txtHt.Location = new System.Drawing.Point(227, 110);
+            this.txtHt.Name = "txtHt";
+            this.txtHt.Size = new System.Drawing.Size(185, 20);
+            this.txtHt.TabIndex = 9;
+            // 
+            // TxtQueQuan
+            // 
+            this.TxtQueQuan.Location = new System.Drawing.Point(227, 212);
+            this.TxtQueQuan.Name = "TxtQueQuan";
+            this.TxtQueQuan.Size = new System.Drawing.Size(185, 20);
+            this.TxtQueQuan.TabIndex = 11;
+            // 
+            // dataTNS
+            // 
+            this.dataTNS.Location = new System.Drawing.Point(227, 161);
+            this.dataTNS.Name = "dataTNS";
+            this.dataTNS.Size = new System.Drawing.Size(185, 20);
+            this.dataTNS.TabIndex = 12;
+            // 
+            // CBBoxLV
+            // 
+            this.CBBoxLV.FormattingEnabled = true;
+            this.CBBoxLV.Location = new System.Drawing.Point(227, 261);
+            this.CBBoxLV.Name = "CBBoxLV";
+            this.CBBoxLV.Size = new System.Drawing.Size(185, 21);
+            this.CBBoxLV.TabIndex = 13;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(513, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(246, 254);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FrmQL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 622);
+            this.Controls.Add(this.CBBoxLV);
+            this.Controls.Add(this.dataTNS);
+            this.Controls.Add(this.TxtQueQuan);
+            this.Controls.Add(this.txtHt);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FrmQL";
+            this.Text = "Quản_Lý";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtHt;
+        private System.Windows.Forms.TextBox TxtQueQuan;
+        private System.Windows.Forms.DateTimePicker dataTNS;
+        private System.Windows.Forms.ComboBox CBBoxLV;
+    }
+}
